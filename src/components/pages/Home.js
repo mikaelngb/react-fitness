e7d86688685449c7749e758661793589544125ee
Mikael <mikaelaja10003@gmail.com>
2023-01-21 03:17:34 +0700

    add few more until 26:11

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+
+import HeroBanner from "../HeroBanner";
+
+import SearchExercises from "../SearchExercises";
+import Exercises from "../Exercises";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
 
 export default Home;
