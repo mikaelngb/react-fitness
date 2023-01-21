7e102b95c5194358c852d04d97e8fc1b2a6aca64
Mikael <mikaelaja10003@gmail.com>
2023-01-22 05:05:33 +0700

    added until 1:16:24

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,48 @@
-import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/banner.png";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <Box
+      sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontStyle="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+      <Typography
+        fontWeight="700"
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px" mb={3}>
+        Check out the most effective exercises
+      </Typography>
+      <Button
+        href="#exercises"
+        variant="contained"
+        color="error"
+        mt={4}
+        sx={{ backgroundColor: "#FF2625", padding: "10px" }}
+      >
+        Explore Exercises
+      </Button>
+      <Typography
+        fontWeight="700"
+        fontSize="200px"
+        color="#FF2625"
+        sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
+      >
+        Exercises
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+    </Box>
+  );
 };
 
 export default HeroBanner;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,93 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
-const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+import { excercisesOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollBar from "./HorizontalScrollBar";
+
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState("");
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        excercisesOptions
+      );
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        excercisesOptions
+      );
+      const searchedExercises = exercisesData.filter((exercise) => {
+        exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search);
+      });
+      setSearch("");
+      setExercises(searchedExercises);
+    }
+  };
+
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exrecises You <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+        />
+        <Button
+          sx={{
+            backgroundColor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "12px" },
+            height: "56px",
+            position: "absolute",
+            right: 0,
+          }}
+          className="search-btn"
+          onClick={() => handleSearch()}
+        >
+          Search
+        </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollBar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
